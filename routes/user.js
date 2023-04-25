bd0cef989b83afdb7436c9c6fcc267f1bf20cb6d
andre.teixeira <andre-x8@hotmail.com>
2023-04-25 20:48:40 -0300

    Criado a rota de login

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,9 @@ const express = require("express");
 const connection = require("../connection");
 const router = express.Router();
 
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
 router.post("/signup", (req, res) => {
   let user = req.body;
   query = "select email,password,role,status from user where email=?";
@@ -23,6 +26,32 @@ router.post("/signup", (req, res) => {
         );
       } else {
         return res.status(400).json({ message: "email já existe" });
+      }
+    } else {
+      return res.status(500).json(err);
+    }
+  });
+});
+
+router.post("/login", (req, res) => {
+  const user = req.body;
+  query = "select  email,password,role,status from user where email=?";
+  connection.query(query, [user.email], (err, results) => {
+    if (!err) {
+      if (results.length <= 0 || results[0].password != user.password) {
+        return res.status(401).json({ message: "Incorreto User ou senha" });
+      } else if (results[0].status === "false") {
+        return res.status(401).json({ message: "Wait for admin approvall" });
+      } else if (results[0].password == user.password) {
+        const response = { email: results[0].email, role: results[0].role };
+        const acecessToken = jwt.sign(response, process.env.ACCESS_TOKEN, {
+          expiresIn: "24h",
+        });
+        res.status(200).json({ token: acecessToken });
+      } else {
+        return res
+          .status(400)
+          .json({ message: "something went wrong. Please try again later" });
       }
     } else {
       return res.status(500).json(err);
