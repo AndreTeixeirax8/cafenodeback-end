b856f993f02c722fc45231116e82389a44891478
andre.teixeira <andre-x8@hotmail.com>
2023-04-29 11:55:26 -0300

    rota de autenticacao concluida

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+/**Projeto feito de acordo com o curso disponivel no youtube
+ * link do curso =
+ * https://www.youtube.com/watch?v=ZHg2u4QNUdw&list=PLdRq0mbeEBmybqNI7hdNPp8RrYaIVkZlW&index=1
+ */
 const express = require("express");
 var cors = require("cors");
 const connection = require("./connection");
