74f4137a48f8ab046944ebaae75ced30081607d9
andre.teixeira <andre-x8@hotmail.com>
2023-05-01 19:06:54 -0300

    adicionado rotas para produto

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var cors = require("cors");
 const connection = require("./connection");
 const userRoute = require("./routes/user");
 const categoryRoute = require("./routes/category");
+const productRoute = require("./routes/product");
 const app = express();
 
 app.use(cors());
@@ -14,5 +15,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/user", userRoute);
 app.use("/category", categoryRoute);
+app.use("/product", productRoute);
 
 module.exports = app;
