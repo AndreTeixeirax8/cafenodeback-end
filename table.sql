7c6fcbd9d200b28afa03783421ad08449866ac2e
AndreTeixeirax8 <andre-x8@hotmail.com>
2023-05-09 20:05:28 -0300

    adicionado table bill

--- table.sql
+++ table.sql
@@ -1,7 +1,7 @@
 create table  user(
     id int primary key AUTO_INCREMENT,
     name varchar(250),
-    contactNumber varchar(20)
+    contactNumber varchar(20),
     email varchar(50),
     password varchar(250),
     status varchar(20),
@@ -27,3 +27,16 @@ create table product(
     status varchar(20),
     primary key(id)
 )
+
+create table bill(
+    id int NOT NULL AUTO_INCREMENT
+    uuid varchar(200) NOT NULL,
+    name varchar(255) NOT NULL,
+    email varchar(255) NOT NULL,
+    contactNumber varchar(20)NOT NULL,
+    paymentMethod varchar(50) NOT NULL,
+    total int NOT NULL,
+    productDetails JSON DEFAULT NULL,
+    createdBy varchar(255) NOT NULL,
+    primary key(id)
+);
