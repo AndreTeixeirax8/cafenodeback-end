d164e5b26595da8b091ec82fd6267ea59d981cf9
AndreTeixeirax8 <andre-x8@hotmail.com>
2023-05-13 16:19:44 -0300

    Iniciado o relátorio está na metade

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const connection = require("./connection");
 const userRoute = require("./routes/user");
 const categoryRoute = require("./routes/category");
 const productRoute = require("./routes/product");
+const billRoute = require('./routes/bill')
 const app = express();
 
 app.use(cors());
@@ -16,5 +17,6 @@ app.use(express.json());
 app.use("/user", userRoute);
 app.use("/category", categoryRoute);
 app.use("/product", productRoute);
+app.use('/bill',billRoute);
 
 module.exports = app;
