5a01ea053b5ee3486f985e3e9d6d52fd457bfab6
AndreTeixeirax8 <andre-x8@hotmail.com>
2023-05-14 12:23:04 -0300

    Rota para deletar relatorio

--- routes/bill.js
+++ routes/bill.js
@@ -90,4 +90,32 @@ router.post('/getPdf',auth.authenticateToken,function(req,res){
 
 })
 
+router.get('/getBills',auth.authenticateToken,(req,res,next)=>{
+   var query = "select * from bill order by id DESC";
+   connection.query(query,(err,results)=>{
+      if(!err){
+        return res.status(200).json(results)
+      }
+      else{
+        return res.status(500).json(err);
+      }
+   })
+})
+
+router.delete('/delete/:id',auth.authenticateToken,(req,res,next)=>{
+  const id = req.params.id;
+  var query = "delete from bill where id=?"
+  connection.query(query,[id],(err,results)=>{
+    if(!err){
+      if(results.affectedRows == 0 ){
+        return res.status(404).json({message:"Relatorio não existe"})
+      }
+      return res.status(200).json({message:"Relatorio apagado com sucesso"})
+    }
+    else{
+      return res.status(500).json(err);
+    }
+  })
+})
+
 module.exports = router;
